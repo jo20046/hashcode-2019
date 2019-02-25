d69e62de0cb5fc8fb91ed33a44f6edf77c9d610b
Enthöfer Joshua <j.enthoefer@laudert.de>
2019-02-25 09:42:35 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("a_example.in")));
+        PrintWriter printWriter = new PrintWriter("a_submit.txt", "UTF-8");
+
+        List<String> list = new ArrayList<>();
+        String s;
+
+        for (; (s = bufferedReader.readLine()) != null; ){
+            list.add(s);
+        }
+        bufferedReader.close();
+
+
+
+
+
+
+
+
+
+
+
+
+        for (int i = 0; i < list.size(); i++) {
+            printWriter.println(list.get(i));
+            System.out.println(list.get(i));
+        }
+        printWriter.close();
+
+    }
+
+
+}
